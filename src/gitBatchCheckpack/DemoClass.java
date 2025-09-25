2874aeae7bfe8b1f88a152705e74435e3499bfe2
Frankduff1505 <frankduff1505@gmail.com>
2025-09-25 12:02:11 +0530

    Update DemoClass.java

--- src/gitBatchCheckpack/DemoClass.java
+++ src/gitBatchCheckpack/DemoClass.java
@@ -6,7 +6,9 @@ public class DemoClass {
 		System.out.println("Java1");
 		System.out.println("GitHub1");
 		System.out.println("GitHub2");
+		System.out.println("GitHub4");
 		System.out.println("Java2");
+		System.out.println("Java3");
 
 	}
 
